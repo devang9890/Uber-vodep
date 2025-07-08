4cdb812f7cced12fb117e73692f3c25bda64cdda
DEVANG SINGH <devang9890@gmail.com>
2025-07-08 13:16:02 +0530

    updated

--- Backend/server.js
+++ Backend/server.js
@@ -4,6 +4,9 @@ const port = process.env.PORT || 3000;
 
 const server = http.createServer(app);
 
+
+//open the local host
 server.listen(port , () =>  {
     console.log(`server is running on port ${port}`);
 });
+
