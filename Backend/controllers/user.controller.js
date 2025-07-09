f23f852683fd3d9a118c5512ef015e8c6821055f
DEVANG SINGH <devang9890@gmail.com>
2025-07-10 02:54:47 +0530

    updated

--- Backend/controllers/user.controller.js
+++ Backend/controllers/user.controller.js
@@ -1,6 +1,28 @@
+const { validationResult } = require('express-validator');
 const userModel = require('../models/user.model');
+const userService = require('../services/user.service');
 
 
-module.exports.registerUser = async(req , resizeBy, next) => {
-    
+module.exports.registerUser = async(req , res, next) => {
+ const errors = validationResult(req);
+ if(!errors.isEmpty){
+    return res.status(400).json({errors:errors.array() });
+
+ }
+
+ const {firstname , lastname , email , password} = req.body;
+
+ const hashedPassword = await userModel.hashedPassword(password);
+
+ const user = await userService.createUser({
+    firstname,
+    lastname,
+    email,
+    password:hashedPassword
+ });
+
+ const token = user.generateAuthToken();
+
+ res.status(201).json({token, user});
+
 }
