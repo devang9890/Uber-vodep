60f53b0d832fca77199284bbd5f7bce2243d8f43
DEVANG SINGH <devang9890@gmail.com>
2025-07-09 14:11:38 +0530

    updated

--- Backend/models/usermodel.js
+++ Backend/models/usermodel.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+
+
 
 
 const userSchema = new mongoose.Schema({
@@ -24,9 +28,32 @@ const userSchema = new mongoose.Schema({
       password :{
         type: String,
         required:true,
+        select : false,
       },
 
       socketId:{
         type:String,
       },
 })
+
+userSchema.methods.generateAuthToken = function() {
+
+  const token = jwt.sign({_id:this._id} , process.env.JWT_SECRET)
+  return token
+
+}
+
+userSchema.methods.comparePassword = async function (password) {
+  return await bcrypt.compare(password , this.password);  
+}
+
+userSchema.statics.hashPassword = async function (password) {
+  return await bcrypt.hash(password, 10);
+  
+}
+
+const userModel = mongoose.model('user' , userSchema );
+
+module.exports = userModel;
+
+
